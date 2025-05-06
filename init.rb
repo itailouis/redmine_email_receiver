f24793bd45542e762796edfd5fcdd76925517335
itai <itaizulu@gmail.com>
2025-05-06 13:36:16 +0200

    setting config file

--- init.rb
+++ init.rb
@@ -1,18 +1,28 @@
 require 'redmine'
 
+def load_settings
+    config_file = File.join(File.dirname(__FILE__), 'config', 'settings.yml')
+    if File.exist?(config_file)
+      YAML.load_file(config_file)['email_settings']
+    else
+      # Default settings if config file doesn't exist
+      {
+        'imap_host' => '',
+        'imap_port' => '25',
+        'imap_ssl' => '0',
+        'imap_username' => '',
+        'imap_password' => '',
+        'imap_folder' => 'INBOX',
+        'default_project' => ''
+      }
+    end
+end
+
 Redmine::Plugin.register :redmine_email_receiver do
   name 'Redmine Email Receiver plugin'
   author 'itai zulu'
   description 'A plugin to receive emails and create issues'
   version '0.0.1'
 
-  settings default: {
-    'imap_host' => '10.170.10.202',
-    'imap_port' => '25',
-    'imap_ssl' => '0',
-    'imap_username' => 'Innovation@fbc.co.zw',
-    'imap_password' => '',
-    'imap_folder' => 'INBOX',
-    'default_project' => 'innovertion'
-  }, partial: 'settings/email_receiver_settings'
+  settings default: load_settings, partial: 'settings/email_receiver_settings'
 end
